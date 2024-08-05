251a5aa9ff907c97b608dac886a22fc4dc192dcc
Jmcortesc <jmcortesc@gmail.com>
2024-08-05 17:07:35 -0500

    Update ejemplo_python.py

--- ejemplo_python.py
+++ ejemplo_python.py
@@ -1,4 +1,5 @@
 # prueba y ensayo usando Github y Python para la ciencia de datos
 
-print("Hola tú, bienvenido a Ciencia de Datos en Github con Python"
+print("Hola tú, bienvenido a Ciencia de Datos en Github con Python")
+
       
